694b955fc0cfd529814ffc8f422689fff137c129
Adama <adamamakhtarsow@gmail.com>
2021-03-09 16:54:52 +0100

    'configureProxy'

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,11 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = app => {
-  app.use(proxy("/api/*", { target: "https://ks-backend.herokuapp.com/" }));
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://ks-backend.herokuapp.com/',
+      changeOrigin: true,
+    })
+  );
 };

49c9bf0abd6824aebef2ba6c7cd94f0655f33bb7
Adama <adamamakhtarsow@gmail.com>
2021-03-11 17:36:50 +0100

    after restoring API

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://ks-backend.herokuapp.com/',
+      target: 'https://keyservicesapi.herokuapp.com/',
       changeOrigin: true,
     })
   );
